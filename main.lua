75bcd8f0d6067f0affa808ed70687977259c2815
E.T <ETdoFresh@gmail.com>
2012-03-01 21:46:56 -0600

    I haave added a moving target and some modes in my physics simulation.

--- main.lua
+++ main.lua
@@ -1,23 +1,44 @@
+display.setStatusBar( display.HiddenStatusBar )  -- hide the status bar
+
+--==============================
+-- Useful local functions
+--==============================
+local function vectAdd(v1, v2)
+	return {x = v1.x + v2.x, y = v1.y + v2.y}
+end
+
+local function vectSub(v1, v2)
+	return {x = v1.x - v2.x, y = v1.y - v2.y}
+end
+
+local function vectMultiply(v1, num)
+	return {x = v1.x * num, y = v1.y * num}
+end
+
+local function vectMagnitude(v1)
+	return math.sqrt(v1.x * v1.x + v1.y * v1.y)
+end
+
+local function vectNormalize(v1)
+	return {x = v1.x / vectMagnitude(v1), y = v1.y / vectMagnitude(v1)}
+end
+
+--==============================
+-- Start of Program
+--==============================
+
 -- Start physics simulation
 local physics = require "physics"
 physics.start()
 
 -- Set properties
 physics.setGravity(0, 0) -- No downward gravity
-physics.setScale(30) --30 pixel/meter (physics works best between 0.1m and 10m)
-physics.setDrawMode("normal") -- drawing mode can be normal, debug, or hybrid
 
 -- Create borders around the edges
 local borderTop = display.newRect(0, 0, display.contentWidth, 1)
 local borderBottom = display.newRect(0, display.contentHeight-1, display.contentWidth, 1)
 local borderLeft = display.newRect(0, 0, 1, display.contentHeight)
 local borderRight = display.newRect(display.contentWidth-1, 1, 1, display.contentHeight)
-
--- name the borders
-borderTop.myName = "borderTop"
-borderBottom.myName = "borderBottom"
-borderLeft.myName = "borderLeft"
-borderRight.myName = "borderRight"
 
 -- add physics to the borders
 local borderBody = {friction=0.4, bounce=0.2}
@@ -26,47 +47,51 @@ physics.addBody(borderBottom, "static", borderBody)
 physics.addBody(borderLeft, "static", borderBody)
 physics.addBody(borderRight, "static", borderBody)
 
--- Create crates!
-local crate1 = display.newRect(50,50,100,100)
-physics.addBody( crate1, { density=3.0, friction=0.5, bounce=0.3 } )
-crate1.myName = "first crate"
- 
-local crate2 = display.newRect(50,200,100,100)
-physics.addBody( crate2, { density=3.0, friction=0.5, bounce=0.3 } )
-crate2.myName = "second crate"
-
--- Create something that happens on collision events
-local function onLocalCollision(self, event)
-	if ( event.phase == "began" ) then
-		print( self.myName .. ": collision began with " .. event.other.myName )
-	elseif ( event.phase == "ended" ) then
-		print( self.myName .. ": collision ended with " .. event.other.myName )
-	end
+--==============================
+-- Object creation
+--==============================
+local target = display.newCircle(10,10,10)
+physics.addBody( target, {density=50, friction=0.5, bounce=0.3, radius=5} )
+target.linearDamping = 0.5
+local function setTarget(event)
+	local v = vectSub(event, target)
+	target:setLinearVelocity(v.x, v.y)
 end
-crate1.collision = onLocalCollision
-crate2.collision = onLocalCollision
-crate1:addEventListener( "collision", crate1 )
-crate2:addEventListener( "collision", crate2 )
 
--- Test how touch events can work with physics
-local function dragBody(event)
-	local body = event.target
-	local stage = display.getCurrentStage()
+Runtime:addEventListener("tap", setTarget)
+
+-- Create a vehicle
+local crate1 = display.newRect(50,50,64,32)
+physics.addBody( crate1, { density=5, friction=0.5, bounce=0.3 } )
+crate1.max_force = 1
+crate1.max_speed = 20
+crate1.mode = "seek"
+
+local function update(event)
+	local velocity = {}
+	velocity.x, velocity.y = crate1:getLinearVelocity()
 	
-	if (event.phase == "began") then
-		stage:setFocus(body)
-		body.isFocus = true
-		body.tempJoint = physics.newJoint("touch", body, event.x, event.y)
-	elseif (body.isFocus) then
-		if (event.phase == "moved") then
-			body.tempJoint:setTarget(event.x, event.y)
-		elseif (event.phase == "ended" or event.phase == "cancelled") then
-			stage:setFocus(nil)
-			body.isFocus = false
-			body.tempJoint:removeSelf()
-		end
+	local vect
+	if (crate1.mode == "flee") then
+		vect = vectSub(crate1, target)
+	elseif (crate1.mode == "seek") then
+		vect = vectSub(target, crate1)
+	else --if (crate1.mode == "pursuit") then
+		local tV = {}
+		tV.x, tV.y = target:getLinearVelocity()
+		tV = vectMultiply(tV, vectMagnitude(target,crate1)/100)
+		tV = vectAdd(target, tV)
+		vect = vectSub(tV, crate1)
+	end
+	local nVect = vectNormalize(vect)
+	local desiredVelocity = vectMultiply(nVect, crate1.max_speed)
+	if (velocity.x ~= desiredVelocity.x or velocity.y ~= desiredVelocity.y) then
+		local steering = vectSub(desiredVelocity, velocity)
+		steering = vectNormalize(steering)
+		steering = vectMultiply(steering, crate1.max_force)
+		local newVelocity = vectAdd(velocity, steering)
+		crate1:setLinearVelocity(newVelocity.x, newVelocity.y)
 	end
 end
 
-crate1:addEventListener("touch", dragBody)
-crate2:addEventListener("touch", dragBody)
+Runtime:addEventListener("enterFrame", update)
