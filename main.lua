7a2244d2bbc20841bf812d3b0ad3e3860431fee0
E.T <ETdoFresh@gmail.com>
2012-03-02 00:23:17 -0600

    Playing with coroutines.

--- main.lua
+++ main.lua
@@ -1,4 +1,44 @@
-local Dog = require 'Dog'
+local myText = display.newText("0", 0, 0, native.systemFont, 32)
+local isTouching = false
+local isWaiting = false
+myText.x = display.contentWidth / 2
+myText.y = display.contentHeight / 2
+myText.val = 0
 
-local dog1 = Dog.new(100,100,20,20)
-local dog2 = Dog.new(150,150,20,20)
+function myCoroutine()
+	for i = 1, 50000 do
+		myText.text = myText.text + 1
+		myText.val = myText.val + 1
+		coroutine.yield()
+	end
+end
+
+function onCoroutine(event)
+	if (not isTouching) then
+		coroutine.resume(co)
+		timer.performWithDelay(5, onCoroutine)
+		isWaiting = false
+	else
+		isWaiting = true
+	end
+end
+
+function onTouch(event)
+	if event.phase == "began" then
+		isTouching = true
+		myText.val = myText.text/1
+	elseif event.phase == "moved" then
+		myText.text = myText.val + (event.yStart - event.y)
+		if myText.text/1 < 0 then myText.text = "0" end
+		print(event.y, event.yStart)
+	elseif event.phase == "ended" then
+		isTouching = false
+		if (isWaiting) then
+			onCoroutine(nil)
+		end
+	end
+end
+
+co = coroutine.create(myCoroutine)
+timer.performWithDelay(5, onCoroutine)
+Runtime:addEventListener("touch", onTouch)
