2d3c592aa9ea3df8bc02b412f7e56bd923821715
E.T <ETdoFresh@gmail.com>
2012-03-03 14:10:30 -0600

    I have made the map scalable. Would like to try to set a target for the map so that the map moves scales towards and moves toward the target.

--- Map.lua
+++ Map.lua
@@ -18,6 +18,9 @@ function Map.new(param)
 	local mapHeight = param.mapHeight or map.height
 	local isFocus = false
 	local prevTime, prevPos, velocity, focusTime
+	local scale = 1
+	local dimension = {width = mapWidth, height = mapHeight}
+	local tween = {x = nil, y = nil}
 	
 	--================================
 	-- Public Variables
@@ -25,14 +28,13 @@ function Map.new(param)
 	map.x = param.x or map.x
 	map.y = param.y or map.y
 	map.rotation = param.rotation or map.rotation
-	map.tween = {x = nil, y = nil}
 	
 	-- These functions will be replaced
 	local superRemoveSelf = map.removeSelf
 	
 	--================================
 	-- Private Functions
-	--================================
+	--================================	
 	local function trackVelocity(event)
 		if (prevPos) then 
 			local timePassed = event.time - prevTime
@@ -62,22 +64,22 @@ function Map.new(param)
 		if (math.abs(delta.x) > 0) then map.x = map.x + delta.x end
 		if (math.abs(delta.y) > 0) then map.y = map.y + delta.y end
 
-		if (not(map.tween.x)) then
+		if (not(tween.x)) then
 			if (map.x > 0) then
 				velocity.x = 0
-				map.tween.x = transition.to(map, {time = 400, x = 0, transition=easing.outQuad, onComplete = function() map.tween.x = nil end})
-			elseif (map.x < display.contentWidth - mapWidth) then
+				tween.x = transition.to(map, {time = 400, x = 0, transition=easing.outQuad, onComplete = function() tween.x = nil end})
+			elseif (map.x < display.contentWidth - dimension.width) then
 				velocity.x = 0
-				map.tween.x = transition.to(map, {time = 400, x = display.contentWidth - mapWidth, transition=easing.outQuad, onComplete = function() map.tween.x = nil end})
+				tween.x = transition.to(map, {time = 400, x = display.contentWidth - dimension.width, transition=easing.outQuad, onComplete = function() tween.x = nil end})
 			end
 		end
-		if (not(map.tween.y)) then
+		if (not(tween.y)) then
 			if (map.y > 0) then
 				velocity.y = 0
-				map.tween.y = transition.to(map, {time = 400, y = 0, transition=easing.outQuad, onComplete = function() map.tween.y = nil end})
-			elseif (map.y < display.contentHeight - mapHeight) then
+				tween.y = transition.to(map, {time = 400, y = 0, transition=easing.outQuad, onComplete = function() tween.y = nil end})
+			elseif (map.y < display.contentHeight - dimension.height) then
 				velocity.y = 0
-				map.tween.y = transition.to(map, {time = 400, y = display.contentHeight - mapHeight, transition=easing.outQuad, onComplete = function() map.tween.y = nil end})
+				tween.y = transition.to(map, {time = 400, y = display.contentHeight - dimension.height, transition=easing.outQuad, onComplete = function() tween.y = nil end})
 			end
 		end
 		
@@ -92,17 +94,17 @@ function Map.new(param)
 			display.getCurrentStage():setFocus(map, event.id)
 			isFocus = true
 			map.x0, map.y0 = event.x, event.y
-			if (map.tween.x) then transition.cancel(map.tween.x) end
-			if (map.tween.y) then transition.cancel(map.tween.y) end
-			map.tween.x = nil
-			map.tween.y = nil
+			if (tween.x) then transition.cancel(tween.x) end
+			if (tween.y) then transition.cancel(tween.y) end
+			tween.x = nil
+			tween.y = nil
 		elseif (isFocus) then
 			if (event.phase == "moved") then
 				local dx = event.x - map.x0
 				local dy = event.y - map.y0
 				map.x0, map.y0 = event.x, event.y
-				if (map.x > 0 or map.x < display.contentWidth - mapWidth) then dx = dx / 2 end
-				if (map.y > 0 or map.y < display.contentHeight - mapHeight) then dy = dy / 2 end				
+				if (map.x > 0 or map.x < display.contentWidth - dimension.width) then dx = dx / 2 end
+				if (map.y > 0 or map.y < display.contentHeight - dimension.height) then dy = dy / 2 end				
 				map.x = map.x + dx
 				map.y = map.y + dy
 			elseif (event.phase == "ended" or event.phase == "cancelled") then
@@ -121,6 +123,48 @@ function Map.new(param)
 	function map:refreshSize()
 		mapWidth = map.width
 		mapHeight = map.height
+	end
+	
+	function map:removeControl()
+		Runtime:removeEventListener("enterFrame", trackVelocity)
+		Runtime:removeEventListener("enterFrame", slowDown)
+		map:removeEventListener("touch", onDrag)
+	end
+	
+	function map:giveControl()
+		map:addEventListener("touch", onDrag)
+	end
+	
+	function map:scale(newScale)
+		-- Get center view point
+		local newX = (map.x - display.contentWidth / 2)
+		local newY = (map.y - display.contentHeight / 2)
+		-- Set back to normal scale
+		newX = newX / scale
+		newY = newY / scale
+		-- calculate new Scale
+		scale = newScale or scale
+		dimension.width = mapWidth * scale 
+		dimension.height = mapHeight * scale
+		-- Scale center view point
+		newX = newX * scale
+		newY = newY * scale
+		-- Get new top left point
+		newX = newX + display.contentWidth / 2
+		newY = newY + display.contentHeight / 2
+		-- Get yo limits right
+		if (newX > 0) then newX = 0
+		elseif (newX < display.contentWidth - dimension.width) then newX = display.contentWidth - dimension.width end
+		if (newY > 0) then newY = 0
+		elseif (newY < display.contentHeight - dimension.height) then newY = display.contentHeight - dimension.height end
+		map:removeControl()
+		-- stop transitions
+		if (tween.x) then transition.cancel(tween.x) end
+		if (tween.y) then transition.cancel(tween.y) end
+		-- make new transitions
+		transition.to(map, {time = 500, x = newX, y = newY, xScale = scale, yScale = scale, onComplete = function()
+			map:giveControl()
+		end})
 	end
 	
 	function map:removeSelf()
--- main.lua
+++ main.lua
@@ -1,5 +1,17 @@
+display.setStatusBar( display.HiddenStatusBar )  -- hide the status bar
+
 local Map = require 'Map'
 
 local map = Map.new{mapWidth = 1920, mapHeight = 1200}
 local image = display.newImageRect(map, "cnh.png", 1920, 1200)
 image.x, image.y = image.width / 2, image.height / 2
+
+local button = display.newRect(10, 10, 40, 25)
+button:setFillColor(255, 0, 0)
+local sizes = {1, 2, 5, 10}
+function button.tap(event)
+	local i = button.size or 1
+	map:scale(i)
+	button.size = (i % #sizes) + 1
+end
+button:addEventListener("tap", button)
